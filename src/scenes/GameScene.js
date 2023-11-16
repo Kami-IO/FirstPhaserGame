420de3d8f637f61f3f7d287c17c46841069c653f
Mattis Riiskjær Bentsen <mattisrbentsen@gmail.com>
2023-11-16 19:45:58 +0100

    Added Pop animation

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,6 +1,12 @@
 import Phaser from 'phaser';
 import CountdownController from './CountdownController';
 import RedBalloon from '../assets/balloon_red_string_ram.png';
+import Explosion1 from '../assets/explosion1.png';
+import Explosion2 from '../assets/explosion2.png';
+import Explosion3 from '../assets/explosion3.png';
+import Explosion4 from '../assets/explosion4.png';
+import Explosion5 from '../assets/explosion5.png';
+import Explosion6 from '../assets/explosion6.png';
 import TitleScreen from './TitleScreen';
 
 
@@ -12,7 +18,7 @@ class GameScene extends Phaser.Scene {
         super();
         // Spawnvariables
         this.lastSpawnTime = 0;
-        this.spawnInterval = 1000; // in milliseconds
+        this.spawnInterval = 200; // in milliseconds
 
         // Timer variables
         this.timer;
@@ -21,6 +27,12 @@ class GameScene extends Phaser.Scene {
     }
     preload() {
         this.load.image('red_balloon', RedBalloon);
+        this.load.image('explosion_image1', Explosion1);
+        this.load.image('explosion_image2', Explosion2);
+        this.load.image('explosion_image3', Explosion3);
+        this.load.image('explosion_image4', Explosion4);
+        this.load.image('explosion_image5', Explosion5);
+        this.load.image('explosion_image6', Explosion6);
     }
 
     create() {
@@ -52,22 +64,86 @@ class GameScene extends Phaser.Scene {
     }
 
     spawnObject() {
-        // Create and position your game object
-        var gameObject = this.add.image(
+        // Create and position your the balloon
+        const balloonObject = this.add.image(
             Phaser.Math.Between(0, this.sys.game.config.width),
             this.sys.game.config.height + 120,
             'red_balloon'
         ).setDepth(1).setScale(0.2);
+  
         
-        this.physics.add.existing(gameObject);
-        gameObject.body.setVelocity(0, -200);
-        gameObject.setInteractive();
-
+        this.physics.add.existing(balloonObject);
+        balloonObject.body.setVelocity(0, -200);
+        balloonObject.setInteractive();
+        
+        // Declare the explosion variable outside the callback
+        let explosion;
         // Listen for the pointerdown event
-        gameObject.on('pointerdown', function () {
-            gameObject.destroy();
-            // Add your custom logic here
+        balloonObject.on('pointerdown', () => {
+            balloonObject.destroy();
+            
+             // Create a new image at the position of the destroyed balloonObject
+        const explosion = this.add.sprite(
+            balloonObject.x,
+            balloonObject.y,
+            'explosion_image1'
+        ).setDepth(1).setScale(0.6);
+        
+        // const explosion2 = this.add.image(
+        //     balloonObject.x,
+        //     balloonObject.y,
+        //     'explosion_image2'
+        // ).setDepth(1).setScale(0.6);
+        // const explosion3 = this.add.image(
+        //     balloonObject.x,
+        //     balloonObject.y,
+        //     'explosion_image3'
+        // ).setDepth(1).setScale(0.6);
+        // const explosion4 = this.add.image(
+        //     balloonObject.x,
+        //     balloonObject.y,
+        //     'explosion_image4'
+        // ).setDepth(1).setScale(0.6);
+        // const explosion5 = this.add.image(
+        //     balloonObject.x,
+        //     balloonObject.y,
+        //     'explosion_image5'
+        // ).setDepth(1).setScale(0.6);
+        // const explosion6 = this.add.image(
+        //     balloonObject.x,
+        //     balloonObject.y,
+        //     'explosion_image6'
+        // ).setDepth(1).setScale(0.6);
+        // Add explosion frames
+        // explosion.addFrame('explosion_image1');
+        // explosion.addFrame('explosion_image1');
+        // explosion.addFrame('explosion_image1');
+        // explosion.addFrame('explosion_image1');
+        // explosion.addFrame('explosion_image1');
+        // explosion.addFrame('explosion_image1');
+
+        // Set up animation
+        this.anims.create({
+            key: 'explode',
+            frames: [
+                { key: 'explosion_image1' },
+                { key: 'explosion_image2' },
+                { key: 'explosion_image3' },
+                { key: 'explosion_image4' },
+                { key: 'explosion_image5' },
+                { key: 'explosion_image6' }
+            ],
+            frameRate: 20, // Number of frames per second
+            repeat: 0 // Play the animation only once
         });
+
+        // Play the animation
+        explosion.on('animationcomplete', () => {
+            explosion.destroy();
+        });
+
+        explosion.play('explode');
+    });
     }
 
     startCountdown() {
